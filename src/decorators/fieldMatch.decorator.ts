dfbd1d21b5aa74f7423e13fa84720458a9a8fed2
muhammadsubhankhan <subhanism@outlook.com>
2025-09-14 17:15:48 +0500

    Add fieldMatch decorator for validation
    - Introduced a new decorator `Match` to validate that two fields match in class-validator.
    - The decorator registers a custom validation rule and provides a default error message for mismatched fields.

---
+++ src/decorators/fieldMatch.decorator.ts
@@ -0,0 +1,26 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+
+export function Match(property: string, validationOptions?: ValidationOptions) {
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      name: 'Match',
+      target: object.constructor,
+      propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const relatedValue = (args.object as any)[args.constraints[0]];
+          return value === relatedValue;
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must match with ${args.constraints[0]}`;
+        },
+      },
+    });
+  };
+}
